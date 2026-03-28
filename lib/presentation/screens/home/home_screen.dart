b18a9b6a9b26908342a8ad4b9c5f4775b41cdd2b
Smit Prajapati <smit.19bemeg100@gmail.com>
2026-03-28 22:50:39 +0530

    Update home screen UI and UX

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,22 +1,40 @@
 import 'dart:ui';
-
-import 'package:bhakti_setu/presentation/screens/event/event_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:bhakti_setu/presentation/screens/event/event_screen.dart';
+import 'package:bhakti_setu/presentation/screens/festival/festival_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final menuItems = <_HomeMenuItem>[
+      _HomeMenuItem(
+        title: "Upcoming Events",
+        icon: Icons.event,
+        builder: (_) => const EventScreen(),
+      ),
+      _HomeMenuItem(
+        title: "Festivals & Muhurat",
+        icon: Icons.festival_rounded,
+        builder: (_) => const FestivalScreen(),
+      ),
+    ];
+
+    final topPadding = MediaQuery.of(context).padding.top + kToolbarHeight + 20;
+
     return Scaffold(
-      backgroundColor: const Color(0xFF121212),
+      backgroundColor: const Color(
+        0xFF0A0A0A,
+      ),
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.black.withOpacity(0.4),
         flexibleSpace: ClipRect(
           child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
             child: Container(color: Colors.transparent),
           ),
         ),
@@ -31,53 +49,180 @@ class HomeScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      extendBodyBehindAppBar: true,
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: GridView.count(
+      body: GridView.builder(
+        padding: EdgeInsets.only(
+          top: topPadding,
+          left: 16,
+          right: 16,
+          bottom: 24,
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           crossAxisSpacing: 16,
           mainAxisSpacing: 16,
-          children: [
-            _buildMenuCard(
-              context,
-              title: "Upcoming Events",
-              icon: Icons.event,
-              screen: EventScreen(),
-            ),
-          ],
+          childAspectRatio: 0.95, 
         ),
+        itemCount: menuItems.length,
+        itemBuilder: (context, index) {
+          final item = menuItems[index];
+          return _PremiumMenuCard(
+            title: item.title,
+            icon: item.icon,
+            pageBuilder: item.builder,
+            index: index,
+          );
+        },
       ),
+    );
+  }
+}
+
+class _PremiumMenuCard extends StatefulWidget {
+  final String title;
+  final IconData icon;
+  final WidgetBuilder pageBuilder;
+  final int index;
+
+  const _PremiumMenuCard({
+    required this.title,
+    required this.icon,
+    required this.pageBuilder,
+    required this.index,
+  });
+
+  @override
+  State<_PremiumMenuCard> createState() => _PremiumMenuCardState();
+}
+
+class _PremiumMenuCardState extends State<_PremiumMenuCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _scaleController;
+  late Animation<double> _scaleAnimation;
+  bool _isHovered = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scaleController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.92).animate(
+      CurvedAnimation(parent: _scaleController, curve: Curves.easeInOut),
     );
   }
 
-  Widget _buildMenuCard(
-    BuildContext context, {
-    required String title,
-    required IconData icon,
-    required Widget screen,
-  }) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => screen),
+  @override
+  void dispose() {
+    _scaleController.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown(TapDownDetails details) => _scaleController.forward();
+  void _onTapUp(TapUpDetails details) => _scaleController.reverse();
+  void _onTapCancel() => _scaleController.reverse();
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 0.0, end: 1.0),
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeOutCubic,
+      builder: (context, value, child) {
+        return Transform.translate(
+          offset: Offset(0, 30 * (1 - value)),
+          child: Opacity(
+            opacity: value,
+            child: child,
+          ),
         );
       },
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, size: 40, color: Colors.orange),
-            const SizedBox(height: 10),
-            Text(title, style: GoogleFonts.poppins(color: Colors.white)),
-          ],
+      child: GestureDetector(
+        onTapDown: _onTapDown,
+        onTapUp: _onTapUp,
+        onTapCancel: _onTapCancel,
+        onTap: () {
+          Future.delayed(const Duration(milliseconds: 150), () {
+            if (mounted) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: widget.pageBuilder),
+              );
+            }
+          });
+        },
+        child: ScaleTransition(
+          scale: _scaleAnimation,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: const LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color(0xFF232323), Color(0xFF151515)],
+              ),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.08),
+                width: 1.5,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  blurRadius: 15,
+                  offset: const Offset(0, 8),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.orange.withOpacity(0.1),
+                    border: Border.all(
+                      color: Colors.orange.withOpacity(0.2),
+                      width: 1,
+                    ),
+                  ),
+                  child: Icon(
+                    widget.icon,
+                    size: 36,
+                    color: Colors.orangeAccent,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Text(
+                    widget.title,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white.withOpacity(0.9),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      letterSpacing: 0.3,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
+}
+
+class _HomeMenuItem {
+  const _HomeMenuItem({
+    required this.title,
+    required this.icon,
+    required this.builder,
+  });
+
+  final String title;
+  final IconData icon;
+  final WidgetBuilder builder;
 }
