cd586ff76f526f4f682c00e869f9328ea116bf4c
Smit Prajapati <smit.19bemeg100@gmail.com>
2026-03-31 19:21:38 +0530

    Create firestore service for donors model

--- lib/data/services/firebase/firestore_service.dart
+++ lib/data/services/firebase/firestore_service.dart
@@ -1,3 +1,4 @@
+import 'package:bhakti_setu/data/models/donor_model.dart';
 import 'package:bhakti_setu/data/models/event_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -9,6 +10,18 @@ class EventFirestoreService {
 
     return snapshot.docs
         .map((doc) => EventModel.fromFirebase(doc.data()))
+        .toList();
+  }
+}
+
+class DonorFirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<List<DonorModel>> getDonors() async {
+    final snapshot = await _firestore.collection("donors").get();
+
+    return snapshot.docs
+        .map((doc) => DonorModel.fromFirestore(doc.data()))
         .toList();
   }
 }
