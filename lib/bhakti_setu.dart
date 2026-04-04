dbd725557f45cb8a0121029999f33136093777d5
Smit Prajapati <smit.19bemeg100@gmail.com>
2026-04-04 19:25:51 +0530

    Navigate Splash screen on the start of the app

--- lib/bhakti_setu.dart
+++ lib/bhakti_setu.dart
@@ -1,6 +1,5 @@
 import 'package:bhakti_setu/core/theme/app_colors.dart';
 import 'package:bhakti_setu/presentation/app_providers.dart';
-import 'package:bhakti_setu/presentation/screens/auth/auth_wrapper.dart';
 import 'package:bhakti_setu/presentation/screens/splash%20screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
