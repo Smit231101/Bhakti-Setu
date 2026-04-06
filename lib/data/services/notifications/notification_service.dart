7576599da79db2a3efa3732b16b990ed5c541aef
Smit Prajapati <smit.19bemeg100@gmail.com>
2026-04-06 19:30:20 +0530

    Create notification service file

---
+++ lib/data/services/notifications/notification_service.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+
+class NotificationService {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  void requestNotificationPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: true,
+      badge: true,
+      carPlay: true,
+      criticalAlert: true,
+      provisional: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      if (kDebugMode) {
+        print("User granted permission");
+      }
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      if (kDebugMode) {
+        print("User granted provisional permission");
+      }
+    } else {
+      if (kDebugMode) {
+        print("User denied permission");
+      }
+    }
+  }
+}
